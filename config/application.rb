3e9c5e13cef46be93867076fde18a508602617c8
g-maiyazu <emujei6123@yahoo.co.jp>
2019-12-28 14:41:06 +0900

    Modify application.rb
    ローカル設定の追加、generatorの設定追加

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,19 @@ module AppName
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.generators do |g|
+      g.test_framework = 'rspec'
+      config.generators.system_tests = false
+      config.generators.stylesheets = false
+      g.helper_specs = false
+      g.view_specs = false
+      g.assets = false
+      g.helper = false
+    end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
