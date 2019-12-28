a9bbf4de2b66780b6f5939b16c656099b80b17f0
g-maiyazu <emujei6123@yahoo.co.jp>
2019-12-28 20:51:05 +0900

    Add file uploader and Modify account-edit function
    画像アップローダー機能を追加、アカウント編集機能を修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  mount_uploader :avatar, AvatarUploader
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :user_name,           presence: true,
                                   uniqueness: true,
                                   length: { maximum: 20 }
