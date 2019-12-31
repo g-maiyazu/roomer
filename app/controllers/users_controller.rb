97929d10de6a2cb908ddaa960f18a2ca81c8feb9
g-maiyazu <emujei6123@yahoo.co.jp>
2020-01-01 06:49:22 +0900

    Add show follow
    フォロー/フォロワー一覧ページの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,13 +31,13 @@ class UsersController < ApplicationController
 
   def following
     @user = User.find(params[:id])
-    @users = @user.following
+    @users = @user.following.page(params[:page]).per(Constants::FollowConut::PAGE)
     render 'show_follow'
   end
 
   def followers
     @user = User.find(params[:id])
-    @users = @user.followers
+    @users = @user.followers.page(params[:page]).per(Constants::FollowConut::PAGE)
     render 'show_follow'
   end
 
