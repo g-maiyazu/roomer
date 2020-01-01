27824522e1f626021c98930f7a7bdd8c297774f0
g-maiyazu <emujei6123@yahoo.co.jp>
2020-01-01 18:01:46 +0900

    Add feed
    フィードの実装

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @latest_posts = Post.page(params[:page]).per(24)
-    @popular_posts = Post.unscoped.joins(:likes).group(:post_id).order(Arel.sql('count(likes.user_id) desc')).page(params[:page]).per(24)
-    @feed_posts = current_user.feed.page(params[:page]).per(12) if user_signed_in?
+    @latest_posts = Post.page(params[:page]).per(9).search(params[:search])
+    @feed_posts = current_user.feed.page(params[:page]).per(5).search(params[:search]) if user_signed_in?
   end
 
   def about
