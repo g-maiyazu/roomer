4d1723eb9905f85e73ad7e3fab8fa2c19111a946
g-maiyazu <emujei6123@yahoo.co.jp>
2020-01-02 14:34:52 +0900

    Modify like
    いいね機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   has_one :profile, dependent: :destroy
   has_many :posts, dependent: :destroy
-  has_many :microposts
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
