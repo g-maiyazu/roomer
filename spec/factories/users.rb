84af2af7b33c29a4f1101b0c60e3cb91661786ca
g-maiyazu <emujei6123@yahoo.co.jp>
2020-01-06 23:02:52 +0900

    Modify user factories
    userのテストデータを修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,7 @@
 FactoryBot.define do
-  factory :user, aliases: [:owner] do
+  factory :user do
     sequence(:user_name) { |n| "tester#{n}" }
     sequence(:email)     { |n| "tester#{n}@example.com" }
     password             { 'password' }
-  end
-
-  trait :invalid do
-    user_name { '' }
-    email     { 'tester@invalid' }
-    password  { 'test' }
   end
 end
