59b6ad8fa5a8b2ef5301474cdea028419a953d93
g-maiyazu <emujei6123@yahoo.co.jp>
2020-01-09 14:22:49 +0900

    Add post spec
    投稿機能スペックの追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     sequence(:user_name) { |n| "tester#{n}" }
     sequence(:email)     { |n| "tester#{n}@example.com" }
     password             { 'password' }
+    confirmed_at { Date.today }
   end
 end
---
+++ spec/system/posts_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :system, js: true do
+  it '新規投稿したあと、その投稿を編集して最後に削除する' do
+    user = FactoryBot.create(:user, user_name: 'test', email: 'test@mail.com')
+    user.create_profile
+
+    # ログイン
+    visit root_path
+
+    click_link 'ログイン'
+    expect(current_path).to eq login_path
+    expect(page).to have_content 'ログイン状態を保持'
+
+    fill_in 'メールアドレス', with: 'test@mail.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログインする'
+    expect(page).to have_content 'testの部屋'
+
+    # 新規投稿
+    click_button 'avatar'
+    click_link '投稿'
+    expect(current_path).to eq new_post_path
+
+    attach_file 'post[image]', "#{Rails.root}/spec/fixtures/room.png", make_visible: true
+    fill_in 'おすすめインテリア情報を入力...', with: '私の部屋は白を基調にしています'
+    expect do
+      click_button '投稿を送信'
+    end.to change(Post, :count).by(1)
+
+    expect(current_path).to eq user_path(user)
+    expect(page).to have_content '投稿されました'
+
+    post = Post.first
+    expect(post.caption).to eq '私の部屋は白を基調にしています'
+    expect(page).to have_link 'a', href: "/posts/#{post.id}"
+
+    click_link 'post-image'
+    expect(current_path).to eq post_path(post)
+    expect(page).to have_content '私の部屋は白を基調にしています'
+  end
+end
