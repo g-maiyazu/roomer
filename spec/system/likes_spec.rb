0fe515f9f6b8b5e7461ca8ffa42ca3a94537868d
g-maiyazu <emujei6123@yahoo.co.jp>
2020-01-09 19:38:08 +0900

    Add relation and like system spec
    フォロー、お気に入り機能のsystemスペック追加

---
+++ spec/system/likes_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe 'Likes', type: :system do
+  it '投稿にいいね/いいね解除する', js: true do
+    post = FactoryBot.create(:post, caption: 'インテイリアにこだわっています')
+    foobar = FactoryBot.create(:user, user_name: 'foobar', email: 'foobar@mail.com')
+    foobar.create_profile
+
+    # ログインする
+    visit root_path
+
+    click_link 'ログイン'
+    expect(current_path).to eq login_path
+    expect(page).to have_content 'ログイン状態を保持'
+
+    fill_in 'メールアドレス', with: 'foobar@mail.com'
+    fill_in 'パスワード', with: 'password'
+    click_button 'ログインする'
+    expect(page).to have_content 'foobarの部屋'
+
+    # 投稿にいいねをする
+    visit post_path(post)
+    expect(page).to have_content 'インテイリアにこだわっています'
+    expect(page).to have_content '0いいね！'
+
+    expect do
+      click_button 'いいね！'
+      expect(page).to have_button '1いいね！'
+    end.to change(post.likes, :count).by(1)
+
+    # 投稿のいいねを解除する
+    visit post_path(post)
+    expect(page).to have_content 'インテイリアにこだわっています'
+
+    expect do
+      click_button 'いいね！'
+      expect(page).to have_button '0いいね！'
+    end.to change(post.likes, :count).by(-1)
+  end
+end
