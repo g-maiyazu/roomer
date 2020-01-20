52f506945345ab06532f3cdbe088472ac0600277
g-maiyazu <emujei6123@yahoo.co.jp>
2020-01-21 00:19:58 +0900

    Setting S3 and modify production deploy setting
    S3の設定を追加、本番環境の設定を修正

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,13 @@
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      # AWS S3
+      :provider              => 'AWS',
+      :region                => ENV['AWS_DEFAULT_REGION'],
+      :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+    config.fog_directory     =  ENV['AWS_S3_BUCKET']
+  end
+end
