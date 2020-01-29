6fa2981c4f6cd55b2657e68a6c99248e6297c95e
g-maiyazu <emujei6123@yahoo.co.jp>
2020-01-30 04:08:48 +0900

    Add aws-ses setting
    aws-sesの設定を追加、各環境でsesを使用するように修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,16 +63,17 @@ Rails.application.configure do
 
   # mailer setting
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :domain => 'smtp.gmail.com',
-    :user_name => ENV['GMAIL_ADDRESS'],
-    :password =>  ENV['GMAIL_PASSWORD'],
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+
+  config.action_mailer.delivery_method = :ses
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address => "smtp.gmail.com",
+  #   :port => 587,
+  #   :domain => 'smtp.gmail.com',
+  #   :user_name => ENV['GMAIL_ADDRESS'],
+  #   :password =>  ENV['GMAIL_PASSWORD'],
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 end
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,7 @@
-ActionMailer::Base.add_delivery_method :ses,
-                                       AWS::SES::Base,
-                                       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-                                       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-                                       server: 'email.us-west-2.amazonaws.com'
+ActionMailer::Base.add_delivery_method(
+  :ses,
+  AWS::SES::Base,
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  server: 'email.us-west-2.amazonaws.com'
+)
