5825d2a0c1053680653ec68404e4c4fb2465977e
g-maiyazu <emujei6123@yahoo.co.jp>
2020-01-29 12:28:55 +0900

    Get rid of unnecessary codes
    不要行やコードを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,6 @@ class User < ApplicationRecord
   has_many :passive_notifications, class_name: 'Notification',
                                    foreign_key: 'visited_id',
                                    dependent: :destroy
-
   mount_uploader :avatar, AvatarUploader
 
   # Include default devise modules. Others available are:
