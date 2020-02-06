d1b0d4f891d81b95b2ba0f4d6ec8c5ff93a73284
g-maiyazu <emujei6123@yahoo.co.jp>
2020-02-07 03:36:15 +0900

    Modify CircleCI

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,10 +1,9 @@
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION}
+cat <<EOF >> ~/.ssh/config
+  User genki
+  ForwardAgent yes
+EOF
 
-MYSECURITYGROUP="sg-02788aaefad3140ce"
-MYIP=`curl -s ifconfig.me`
+eval $(ssh-agent)
+ssh-add
 
-aws ec2 authorize-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
 bundle exec cap production deploy
-aws ec2 revoke-security-group-ingress --group-id $MYSECURITYGROUP --protocol tcp --port 22 --cidr $MYIP/32
