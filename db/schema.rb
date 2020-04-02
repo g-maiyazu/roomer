6001ec28c72c6d315fa385fdfcda277facd4119a
g-maiyazu <emujei6123@yahoo.co.jp>
2020-04-02 12:42:42 +0900

    Add index to user
    ユーザーモデルのユーザー名にインデックスを付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_135007) do
+ActiveRecord::Schema.define(version: 2020_04_02_033547) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -127,6 +127,7 @@ ActiveRecord::Schema.define(version: 2020_01_15_135007) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
   add_foreign_key "comments", "posts"
